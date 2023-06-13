459762261d42b69c610adcc0a09b4770179f9fb5
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-06-13 11:03:29 +1000

    Add cluster handlers

--- spark_connect_labextension/handlers/handlers.py
+++ spark_connect_labextension/handlers/handlers.py
@@ -3,6 +3,7 @@ import json
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
+from spark_connect_labextension.handlers.cluster_handler import StartClusterRouteHandler, StopClusterRouteHandler, GetClusterLogRouteHandler
 
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
@@ -18,7 +19,15 @@ class RouteHandler(APIHandler):
 def setup_handlers(web_app):
     host_pattern = ".*$"
 
-    base_url = web_app.settings["base_url"]
-    route_pattern = url_path_join(base_url, "spark-connect-labextension", "get-example")
-    handlers = [(route_pattern, RouteHandler)]
+    
+    handlers = [
+        (get_pattern(web_app, "get-example"), RouteHandler),
+        (get_pattern(web_app, "cluster/start"), StartClusterRouteHandler),
+        (get_pattern(web_app, "cluster/stop"), StopClusterRouteHandler),
+        (get_pattern(web_app, "cluster/logs"), GetClusterLogRouteHandler),
+    ]
     web_app.add_handlers(host_pattern, handlers)
+
+def get_pattern(web_app, path):
+    base_url = web_app.settings["base_url"]
+    return url_path_join(base_url, "spark-connect-labextension", path)
--- spark_connect_labextension/sparkconnectserver/spawner.py
+++ spark_connect_labextension/sparkconnectserver/spawner.py
@@ -1,25 +1,45 @@
 import os
 import subprocess
+import tempfile
+import glob
+from enum import Enum
 
 SPARK_HOME = os.getenv('SPARK_HOME')
 SPARK_CONNECT_PACKAGE = os.getenv('SPARK_CONNECT_PACKAGE', 'org.apache.spark:spark-connect_2.12:3.4.0')
 
 class SparkConnectCluster:
-    def __init__(self):
-        self.running = False
+    class Status(Enum):
+        STOPPED = "STOPPED"
+        PROVISIONING = "PROVISIONING"
+        READY = "READY"
 
-    def start(self):
+    def __init__(self):
+        self.status = SparkConnectCluster.Status.STOPPED
+        self.tmpdir = None
+
+    def start(self, options: dict = None, envs: dict = None):
         print("Starting Spark Connect server...")
-        run_script = f"{SPARK_HOME}/sbin/start-connect-server.sh --packages {SPARK_CONNECT_PACKAGE}"
-        retcode = subprocess.Popen(run_script, shell=True).wait()
+
+        self.tmpdir = tempfile.TemporaryDirectory()
+        env_variables = self.get_envs(envs)
+        env_variables['SPARK_LOG_DIR'] = self.tmpdir.name
+        print("Spark log dir", self.tmpdir.name)
+
+        config_args = self.get_config_args(options)
+        run_script = f"{SPARK_HOME}/sbin/start-connect-server.sh --packages {SPARK_CONNECT_PACKAGE} {config_args}"
+        print(run_script)
+        retcode = subprocess.Popen(run_script, shell=True, env=env_variables).wait()
         if retcode != 0:
             raise Exception("Cannot start Spark Connect server")
         
-        self.running = True
-        
-    
+        self.status = SparkConnectCluster.Status.PROVISIONING
+
     def stop(self):
-        if self.running:
+        if self.tmpdir:
+            self.tmpdir.cleanup()
+            self.tmpdir = None
+
+        if self.status == SparkConnectCluster.Status.READY:
             print("No running Spark Connect server")
             return
 
@@ -29,10 +49,37 @@ class SparkConnectCluster:
         if retcode != 0:
             raise Exception("Cannot stop Spark Connect server")
         
-        self.running = False
+        self.status = SparkConnectCluster.Status.STOPPED
+    
+    def get_log(self):
+        if not self.tmpdir:
+            return None
+        
+        files = glob.glob(self.tmpdir.name + '/*.out')
+        logfile = files[-1]
+        with open(logfile, 'r') as f:
+            return f.read()
+ 
+    
+    def get_envs(self, envs: dict):
+        my_env = os.environ.copy()
+        if envs:
+            for key in envs:
+                my_env[key] = envs[key]
+        return my_env
+
+    def get_config_args(self, options: dict):
+        if not options:
+            return []
+        
+        args = []
+        for key in options:
+            args.append("--conf")
+            val = options[key].replace(' ', '\\ ')
+            args.append(key + '=' + val)
+        return ' '.join(args)
+
     
     def __del__(self):
-        if self.running:
+        if self.status != SparkConnectCluster.Status.STOPPED:
             self.stop()
-    
-    
