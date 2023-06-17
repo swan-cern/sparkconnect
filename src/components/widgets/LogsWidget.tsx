5a5c323a31deee8a63680a264214af347d9eae4b
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-06-17 11:53:17 +1000

    Disable refresh interval

--- src/components/widgets/LogsWidget.tsx
+++ src/components/widgets/LogsWidget.tsx
@@ -4,7 +4,7 @@ import CodeMirror from '@uiw/react-codemirror';
 import { jupyterTheme } from '@jupyterlab/codemirror';
 
 const LogsWidget: React.FC = () => {
-  const { data } = useSWR('/cluster/logs', { refreshInterval: 500 });
+  const { data } = useSWR('/cluster/logs');
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', width: '100%', height: '100%' }}>
