f8cf1fce59850b615135495ad358da145d7563a4
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-06-22 00:40:40 +1000

    Ensure one spark session per kernel

--- spark_connect_labextension/kernelextension.py
+++ spark_connect_labextension/kernelextension.py
@@ -1,12 +1,22 @@
 from pyspark.sql import SparkSession
 from .config import SPARK_CONNECT_PORT
 
-def get_spark_session():
-    spark = SparkSession.builder \
-          .remote(f"sc://127.0.0.1:{SPARK_CONNECT_PORT}") \
-          .getOrCreate()
-    return spark
-
 
 def load_ipython_extension(ipython):
-    ipython.push({'get_spark_session': get_spark_session})
+    ipython._spark_conect_labextension__session = None
+
+    def get_spark_session():
+        if not ipython._spark_conect_labextension__session:
+            ipython._spark_conect_labextension__session = SparkSession.builder \
+                .remote(f"sc://127.0.0.1:{SPARK_CONNECT_PORT}") \
+                .getOrCreate()
+        return ipython._spark_conect_labextension__session
+
+    def reset_spark_session():
+        ipython._spark_conect_labextension__session = None
+        return get_spark_session()
+    
+    ipython.push({
+        'get_spark_session': get_spark_session,
+        'reset_spark_session': reset_spark_session
+    })
