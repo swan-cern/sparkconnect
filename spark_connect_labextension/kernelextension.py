c4abb300386ae439b545ce15937c4588df64959f
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-06-22 10:01:34 +1000

    Use module global variable

--- spark_connect_labextension/kernelextension.py
+++ spark_connect_labextension/kernelextension.py
@@ -1,21 +1,22 @@
 from pyspark.sql import SparkSession
 from .config import SPARK_CONNECT_PORT
 
+spark_session = None
+
+def get_spark_session():
+    global spark_session
+    if not spark_session:
+        spark_session = SparkSession.builder \
+            .remote(f"sc://127.0.0.1:{SPARK_CONNECT_PORT}") \
+            .getOrCreate()
+    return spark_session
+
+def reset_spark_session():
+    global spark_session
+    spark_session = None
+    return get_spark_session()
 
 def load_ipython_extension(ipython):
-    ipython._spark_conect_labextension__session = None
-
-    def get_spark_session():
-        if not ipython._spark_conect_labextension__session:
-            ipython._spark_conect_labextension__session = SparkSession.builder \
-                .remote(f"sc://127.0.0.1:{SPARK_CONNECT_PORT}") \
-                .getOrCreate()
-        return ipython._spark_conect_labextension__session
-
-    def reset_spark_session():
-        ipython._spark_conect_labextension__session = None
-        return get_spark_session()
-    
     ipython.push({
         'get_spark_session': get_spark_session,
         'reset_spark_session': reset_spark_session
