1c77d566c77922f637054f44ba1074de6e054bfc
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-07-03 09:11:21 +1000

    Add retrieve config bundles from file

--- spark_connect_labextension/handlers/base.py
+++ spark_connect_labextension/handlers/base.py
@@ -1,5 +1,7 @@
+import json
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.extension.handler import ExtensionHandlerMixin
+from jsonpath_ng import jsonpath, parse
 
 class SparkConnectAPIHandler(ExtensionHandlerMixin, APIHandler):
     @property
@@ -12,11 +14,35 @@ class SparkConnectAPIHandler(ExtensionHandlerMixin, APIHandler):
     
     @property
     def spark_config_bundles(self):
-        return self.ext_config['config_bundles']
+        config_bundles = self.ext_config.get('config_bundles', {})
+        from_file_options = self.ext_config.get('config_bundles_from_file')
+        if from_file_options:
+            file_path = from_file_options['file']
+            with open(file_path, 'r') as f:
+                json_data = json.load(f)
+                jsonpath_expr = parse(from_file_options['json_path'])
+                matches = jsonpath_expr.find(json_data)
+                first_match = matches[0]
+                out_config_bundles = {**config_bundles, **first_match.value}
+                config_bundles = out_config_bundles
+            
+        return config_bundles
     
     @property
     def spark_options(self):
-        return self.ext_config['spark_options']
+        options = self.ext_config.get('spark_options', [])
+        from_file_options = self.ext_config.get('spark_options_from_file')
+        if from_file_options:
+            file_path = from_file_options['file']
+            with open(file_path, 'r') as f:
+                json_data = json.load(f)
+                jsonpath_expr = parse(from_file_options['json_path'])
+                matches = jsonpath_expr.find(json_data)
+                first_match = matches[0]
+                out_options = options + first_match.value
+                options = out_options
+
+        return options
     
     @property
     def error_suggestions(self):
