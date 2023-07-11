7b5d5326698cb2a4de438c48190a3f56baa6e448
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-07-11 23:16:58 +1000

    Change import points

--- spark_connect_labextension/__init__.py
+++ spark_connect_labextension/__init__.py
@@ -1,5 +1,4 @@
 from ._version import __version__
-from .app import SparkConnectExtensionApp
 from .config import EXTENSION_ID
 
 
@@ -11,24 +10,24 @@ def _jupyter_labextension_paths():
 
 
 def _jupyter_server_extension_points():
+    from .app import SparkConnectExtensionApp
     return [{
         "module": "spark_connect_labextension",
         "app": SparkConnectExtensionApp
     }]
 
 
-# def _load_jupyter_server_extension(server_app):
-#     """Registers the API handler to receive HTTP requests from the frontend extension.
+def _load_jupyter_server_extension(server_app):
+    from .app import SparkConnectExtensionApp
+    """Registers the API handler to receive HTTP requests from the frontend extension.
 
-#     Parameters
-#     ----------
-#     server_app: jupyterlab.labapp.LabApp
-#         JupyterLab application instance
-#     """
-#     setup_handlers(server_app.web_app)
-#     name = "spark_connect_labextension"
-#     server_app.log.info(f"Registered {name} server extension")
+    Parameters
+    ----------
+    server_app: jupyterlab.labapp.LabApp
+        JupyterLab application instance
+    """
+    SparkConnectExtensionApp.load_classic_server_extension(server_app)
 
 
 # For backward compatibility with notebook server - useful for Binder/JupyterHub
-load_jupyter_server_extension = SparkConnectExtensionApp.load_classic_server_extension
+# load_jupyter_server_extension = SparkConnectExtensionApp.load_classic_server_extension
