d2b739e5e4ea48d7ca66de54ccc7b2d59df20023
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-07-13 23:26:25 +1000

    Remove copy button

--- src/components/ready/CodePreview.tsx
+++ src/components/ready/CodePreview.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { copyIcon, addIcon } from '@jupyterlab/ui-components';
+import React from 'react';
+import { addIcon } from '@jupyterlab/ui-components';
 import { NotebookActions } from '@jupyterlab/notebook';
 import CodeMirror from '@uiw/react-codemirror';
 import { jupyterTheme } from '@jupyterlab/codemirror';
@@ -13,7 +13,6 @@ spark = get_spark_session()
 `.trim();
 
 export default function CodePreview() {
-  const [copied, setCopied] = useState<boolean>(false);
   const { data } = useStatus();
   const port = data?.port ?? 15002;
   const code = CODE.replace('{PORT}', `${port}`);
@@ -45,19 +44,6 @@ export default function CodePreview() {
         />
       </div>
       <div style={{ marginTop: 4, display: 'flex', flexDirection: 'row', justifyContent: 'end' }}>
-        <button
-          className="jp-ToolbarButtonComponent jp-mod-minimal jp-Button"
-          onClick={() => {
-            navigator.clipboard.writeText(CODE);
-            setCopied(true);
-            setTimeout(() => setCopied(false), 2000);
-          }}
-        >
-          <copyIcon.react tag="span" width={16} height={16} marginTop={4} />{' '}
-          <span className="jp-ToolbarButtonComponent-label" style={{ color: 'var(--jp-ui-font-color2)' }}>
-            {copied ? 'Copied!' : 'Copy'}
-          </span>
-        </button>
         {!!activeNotebookPanel && (
           <button className="jp-ToolbarButtonComponent jp-mod-minimal jp-Button" onClick={addCellToNotebook}>
             <addIcon.react tag="span" width={16} height={16} marginTop={4} />{' '}
