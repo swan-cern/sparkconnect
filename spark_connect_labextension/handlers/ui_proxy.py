c429731da7366835cac1d6f1c76b7a8c985501e9
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-07-15 01:19:49 +1000

    Set webui host to hostname

--- spark_connect_labextension/handlers/ui_proxy.py
+++ spark_connect_labextension/handlers/ui_proxy.py
@@ -1,38 +1,39 @@
 import socket
 from jupyter_server_proxy.handlers import ProxyHandler
-from jupyter_server.extension.handler import ExtensionHandlerMixin
+# from jupyter_server.extension.handler import ExtensionHandlerMixin
 from spark_connect_labextension.config import EXTENSION_ID
 from spark_connect_labextension.sparkconnectserver.cluster import cluster
 
 
-class SparkUIProxyHandler(ExtensionHandlerMixin, ProxyHandler):
-    @property
-    def ext_config(self):
-        return self.settings['spark_connect_config']
+class SparkUIProxyHandler(ProxyHandler):
+    # @property
+    # def ext_config(self):
+    #     return self.settings['spark_connect_config']
     
-    @property
-    def spark_clusters(self):
-        return self.ext_config['clusters']
+    # @property
+    # def spark_clusters(self):
+    #     return self.ext_config['clusters']
     
     @property
     def spark_webui_host(self):
-        current_cluster = self.spark_clusters[cluster.cluster_name]
-        return current_cluster.get('webui_hostname', socket.gethostname())
+        # current_cluster = self.spark_clusters[cluster.cluster_name]
+        # return current_cluster.get('webui_hostname', socket.gethostname())
+        return socket.gethostname()
     
     @property
     def spark_webui_port(self):
-        current_cluster = self.spark_clusters[cluster.cluster_name]
-        return current_cluster.get('webui_port', 4040)
+        # current_cluster = self.spark_clusters[cluster.cluster_name]
+        return 4040
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.proxy_base = f'/{EXTENSION_ID}/ui'
+        self.host_allowlist = [self.spark_webui_host]
 
     async def http_get(self, proxied_path):
         return await self.proxy(proxied_path)
 
     async def open(self, proxied_path):
-        self.host_allowlist = [self.spark_webui_host]
         print("Host open allowlist", self.host_allowlist)
         return await super().proxy_open(self.spark_webui_host, self.spark_webui_port, proxied_path)
 
@@ -55,6 +56,5 @@ class SparkUIProxyHandler(ExtensionHandlerMixin, ProxyHandler):
         return self.proxy(proxied_path)
 
     def proxy(self, proxied_path):
-        self.host_allowlist = [self.spark_webui_host]
         print("Host allowlist", self.host_allowlist)
         return super().proxy(self.spark_webui_host, self.spark_webui_port, proxied_path)
